5a714cc9205a6621f30a7135b704176a903315e9
thunder-hammer <ds.ds.ds.ds.ds.ds10@gmail.com>
2019-03-28 14:58:46 -0600

    added an example edif and updated the example program to use it.

--- Example.py
+++ Example.py
@@ -1,4 +1,5 @@
 from spydrnet.parsers.edif.parser import EdifParser
 
-edif_parser = EdifParser()
-edif_parser.run()
+edif_parser = EdifParser.from_filename(r"data\json_edif\temp.edf")
+edif_parser.parse()
+print(edif_parser.netlist)
