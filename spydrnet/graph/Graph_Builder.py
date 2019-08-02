30f15a1e774cf6921b1ee9e135474e2b9c7427d2
Michael <michael.bjerregaard14@gmail.com>
2019-08-02 14:36:21 -0600

    Refactor of God

--- spydrnet/graph/Graph_Builder.py
+++ spydrnet/graph/Graph_Builder.py
@@ -4,6 +4,8 @@ from spydrnet.ir import *
 from spydrnet.utility.HierarchicalLookup import HierarchicalLookup
 from spydrnet.utility.utility import Utility
 import spydrnet.utility.utility as util
+import spydrnet.support_files as files
+from spydrnet.utility.Uniqueifier import Uniquifier
 
 
 import networkx as nx
@@ -32,18 +34,15 @@ class GraphBuilder:
                 downstream_leaf_cells = self.get_downstream_leaf_cells(instance)
                 for downstream_leaf_cell in downstream_leaf_cells:
                     self.ir_graph.add_edge(instance, downstream_leaf_cell)
-                    # self.ir_graph.add_edge(instance['EDIF.identifier'], downstream_leaf_cell['EDIF.identifier'])
                 other_dictionary = dict()
                 other_dictionary['type'] = instance.definition['EDIF.identifier']
                 my_dictionary[instance] = other_dictionary
                 visited.add(leaf_cell)
         nx.set_node_attributes(self.ir_graph, my_dictionary)
         self._build_sequential_graph()
-        # self.show_graph()
 
     def _build_sequential_graph(self):
         new_graph = nx.DiGraph()
-        # self.show_graph(self.ir_graph)
         for node in self.ir_graph.nodes():
             if util.is_combinational(node):
                 continue
@@ -57,7 +56,6 @@ class GraphBuilder:
                     continue
                 downstream = downstream.union(self._get_successors(downstream_stream_node))
         self.sequential_graph = new_graph
-        # self.show_graph(self.sequential_graph)
 
     def _get_successors(self, node):
         successors = set()
@@ -67,7 +65,6 @@ class GraphBuilder:
 
     def get_downstream_leaf_cells(self, leaf_cell):
         pins = list()
-        # util = Utility()
         hierarchical_name = util.get_hierarchical_name(leaf_cell)
         instance_trace = self.lookup.get_instance_from_name(hierarchical_name)
         instance_trace.pop()
@@ -115,8 +112,27 @@ from spydrnet.parsers.edif.parser import EdifParser
 
 if __name__ == '__main__':
     parser = EdifParser.from_filename("fourBitCounter.edf")
+    parser = EdifParser.from_filename(files.edif_files['unique_challenge.edf'])
     parser.parse()
+    uniquifier = Uniquifier()
     ir = parser.netlist
+    # uniquifier.run(ir)
     builder = GraphBuilder()
     builder.build_graph(ir)
+    ir_graph = builder.ir_graph
+    print(nx.info(ir_graph))
+    uniquifier.run(ir)
+    builder = GraphBuilder()
+    builder.build_graph(ir)
+    ir_graph = builder.ir_graph
+    print(nx.info(ir_graph))
+    new_graph = nx.DiGraph()
+    i = 0
+    for node in ir_graph.nodes:
+        i += 1
+        for successor in ir_graph.successors(node):
+            new_graph.add_edge(node['EDIF.identifier'], successor['EDIF.identifier'])
+    nx.draw(new_graph, with_labels=True)
+    print(nx.info(new_graph))
+    plt.show()
     # builder.show_graph()
