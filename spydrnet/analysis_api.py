5cfcbdfd1d82c936fa85bda6301d85444196beab
thunder-hammer <dmsisawesome@gmail.com>
2019-10-02 13:16:54 -0600

    added file for analysis api

---
+++ spydrnet/analysis_api.py
@@ -0,0 +1,16 @@
+"""
+From the specification for version 1.0:
+
+Analysis API calls:
+get_designs
+get_cells
+get_ports
+get_cables
+get_libraries
+get_properties
+
+Of cells of instance of ...
+
+
+These calls are collected/imported/defined here
+"""
