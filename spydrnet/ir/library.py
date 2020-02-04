251a4aae861925635a3bbc7b98dc466860ada6bf
thunder-hammer <dmsisawesome@gmail.com>
2020-02-04 16:23:48 -0700

    changed signature for callbacks

--- spydrnet/ir/library.py
+++ spydrnet/ir/library.py
@@ -70,7 +70,7 @@ class Library(Element):
         """
         assert definition.library is not self, "Definition already included in library"
         assert definition.library is None, "Definition already belongs to a different library"
-        global_callback._call_library_add_definition(self, definition, position = position)
+        global_callback._call_library_add_definition(self, definition)
         if position is not None:
             self._definitions.insert(position, definition)
         else:
