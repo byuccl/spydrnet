4de19f93deac29002f82e882fe5ccfaee825d880
thunder-hammer <dmsisawesome@gmail.com>
2020-02-26 14:13:11 -0700

    added tests to the uniquify related to issue #78

--- spydrnet/uniquify.py
+++ spydrnet/uniquify.py
@@ -12,27 +12,39 @@ uniqify -- makes all definitions unique below the top instance. definitions that
 mod_name_uid = 0
 def _get_unique_name_modifier():
     global mod_name_uid
-    str_out = str(mod_name_uid)
+    str_out = "_sdn_unique_" + str(mod_name_uid)
     mod_name_uid += 1
     return str_out
 
 def _make_instance_unique(instance):
     '''clone the definition and point the reference to the new definition'''
-    lib = instance.reference.parent
-    name = instance.reference["NAME"]
-    instance.reference = instance.reference.clone()
-    instance.reference['NAME'] = name + _get_unique_name_modifier()
-
+    lib = instance.reference.library
+    new_def = instance.reference.clone()
+    if instance.reference.name != None:
+        name = instance.reference.name
+        new_def.name = name + _get_unique_name_modifier()
+    lib.add_definition(new_def)
+    instance.reference = new_def
 
 def _is_unique(instance):
     '''return if the instance is the only one of its kind in the definition'''
     return len(instance.reference.references) == 1 or instance.reference.is_leaf()
 
-def uniqify(netlist):
-    #TODO remove this line:
-    
-    
+def uniquify(netlist):
+    '''make the instances in the netlist unique
+    uniqification is done in place. Each instance will correspond to exactly one definition and each definition will correspond to exactly one instance with the exception of leaf cells.
+    Leaf cells are can be instanced unlimited numbers of times. Any netlist elements that are not instantiated by the top instance will not be modified and may retain duplicate instances
+    Currently there is no guarantee that the original definition names will be maintained, but it is guaranteed that they will be unique within the scope of all hardware that is below the top instance.
 
+    Renameing is predictable. the string: _sdn_unique_# will be added to the end of the definition names.
+    
+    parameter - netlist, the netlist that will be uniquified
+
+    returns - no returns
+
+    '''
+
+    #import pdb; pdb.set_trace()
     #starting with top.
     #top must be unique below top. otherwise we have infinite harware recursion which is does not make much sense.    
     instance_queue = deque()
@@ -42,8 +54,8 @@ def uniqify(netlist):
     top_definition = top_instance.reference
 
     #put all of tops children on a stack
-    for ref in top_definition.references:
-        instance_queue.append(ref)
+    for chld in top_definition.children:
+        instance_queue.append(chld)
 
     #for each of the children on the stack
     while len(instance_queue) > 0:
@@ -53,5 +65,5 @@ def uniqify(netlist):
             #uniquify
             _make_instance_unique(inst)
         #put their children on the stack
-        for ref in inst.reference.references:
-            instance_queue.append(ref)
+        for chld in inst.reference.children:
+            instance_queue.append(chld)
