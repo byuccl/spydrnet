83fdc522fac1ec12fa097c62bf5fd6151cb96070
thunder-hammer <dmsisawesome@gmail.com>
2020-04-09 23:59:29 -0600

    added some tests. this should work but there will be cables which are disconnected and just floating there at the top level. The tests need a little more work to be automated. right now you need to look at the output to assess how got they are

--- spydrnet/flatten.py
+++ spydrnet/flatten.py
@@ -23,12 +23,12 @@ def _rename_element(e):
     e.name = e.name + "_flat_" + str(unique_number)
     unique_number += 1
 
-def _redo_connections(inst):
-    for pin in inst.pins:
-        out_wire = pin.wire
-        in_pin = pin.inner_pin
+def _redo_connections(instance, port):
+    for pin in port.pins:
+        out_pin = instance.pins[pin]
+        out_wire = out_pin.wire
+        in_pin = pin
         in_wire = in_pin.wire
-        in_wire = Wire()
         pins_to_move = []
         for p in in_wire.pins:
             if p != pin:
@@ -36,43 +36,50 @@ def _redo_connections(inst):
         for p in pins_to_move:
             in_wire.disconnect_pin(p)
             out_wire.connect_pin(p)
-        out_wire.disconnect_pin(pin)
+        out_wire.disconnect_pin(out_pin)
+        in_wire.disconnect_pin(in_pin)
 
-def _bring_to_top_cable(c,top_definition):
+def _bring_to_top_cable(c,inst,top_definition):
     '''move the cable that is internal to the top level.'''
     d = c.definition
-    add_to_name = d.name
+    add_to_name = inst
     d.remove_cable(c)
     #_rename_element(c)
-    c.name = add_to_name + "/" + c.name
-    top_definition.add_child(c)
+    if(add_to_name != ''):
+        c.name = add_to_name + "/" + c.name
+    else:
+        c.name = c.name
+    top_definition.add_cable(c)
 
-def _bring_to_top_inst(i, top_definition):
+def _bring_to_top_inst(i,inst,top_definition):
     '''move the instance that is internal to the top level.'''
     #just remove the instance/cable from the definition to which it belongs then add it to the top definition
     d = i.parent
-    add_to_name = d.name
+    add_to_name = inst
     d.remove_child(i)
     #_rename_element(i)
-    i.name = add_to_name + "/" + i.name
+    if(add_to_name != ''):
+        i.name = add_to_name + "/" + i.name
+    else:
+        i.name = i.name
     top_definition.add_child(i)
     
-def simple_recursive_netlist_visualizer(netlist):
-    #TODO put this code somewhere where people can use it to visualize simple netlists
-    top_instance = netlist.top_instance
-    #should look something like this:
-    #top
-    #   child1
-    #       child1.child
-    #   child2
-    #       child2.child
-    def recurse(instance, depth):
-        s = depth * "\t"
-        print(s, instance.name, "(", instance.reference.name, ")")
-        for c in instance.reference.children:  
-            recurse(c, depth + 1)
+# def simple_recursive_netlist_visualizer(netlist):
+#     #TODO put this code somewhere where people can use it to visualize simple netlists
+#     top_instance = netlist.top_instance
+#     #should look something like this:
+#     #top
+#     #   child1
+#     #       child1.child
+#     #   child2
+#     #       child2.child
+#     def recurse(instance, depth):
+#         s = depth * "\t"
+#         print(s, instance.name, "(", instance.reference.name, ")")
+#         for c in instance.reference.children:  
+#             recurse(c, depth + 1)
     
-    recurse(top_instance, 0)
+#     recurse(top_instance, 0)
 
 def flatten(netlist):
     '''
@@ -82,30 +89,34 @@ def flatten(netlist):
 
     #get all the sub instances of the top instance
     instance_queue = deque()
+    name_queue = deque()
     top_instance = netlist.top_instance
     top_definition = top_instance.reference
     #put all of tops children on a stack
     for chld in top_definition.children:
         instance_queue.append(chld)
+        name_queue.append('')
 
     to_remove = []
     #for each of the children on the stack
     while len(instance_queue) > 0:
         inst = instance_queue.popleft()
+        parent_name = name_queue.popleft()
         # simple_recursive_netlist_visualizer(netlist)
-        _bring_to_top_inst(inst, top_definition)
+        _bring_to_top_inst(inst, parent_name, top_definition)
         if inst.reference.is_leaf():
             continue
         #put their children on the stack
         for child in inst.reference.children:
             instance_queue.append(child)
+            name_queue.append(inst.name)
         temp_cables = []
         for cable in inst.reference.cables:
             temp_cables.append(cable)
         for cable in temp_cables:
-            _bring_to_top_cable(cable, top_definition)
-        
-        _redo_connections(inst)
+            _bring_to_top_cable(cable, inst.name, top_definition)
+        for port in inst.reference.ports:
+            _redo_connections(inst, port)
         to_remove.append(inst)
     for i in to_remove:
         top_definition.remove_child(i)
--- spydrnet/tests/test_flatten.py
+++ spydrnet/tests/test_flatten.py
@@ -1,7 +1,7 @@
 
 import unittest
 import spydrnet as sdn
-from spydrnet import Netlist, Instance, Definition
+from spydrnet.ir import *
 from spydrnet.flatten import flatten
 from spydrnet.uniquify import uniquify
 
@@ -49,6 +49,169 @@ class TestUniquify(unittest.TestCase):
         nl.top_instance.name = ("top_instance")
         return nl
 
+    def create_mem_bus(self,top, proc, mem):
+        mem_width = 10
+        mem_write_out = proc.create_port()
+        mem_write_out.name = "mem_write_out"
+        mem_write_out.create_pins(mem_width)
+        mem_read_in = proc.create_port()
+        mem_read_in.name = "mem_read_in"
+        mem_read_in.create_pins(mem_width)
+
+        mem_write_in = mem.create_port()
+        mem_write_in.name = "mem_write_in"
+        mem_write_in.create_pins(mem_width)
+        mem_read_out = mem.create_port()
+        mem_read_out.name = "mem_read_out"
+        mem_read_out.create_pins(mem_width)
+
+        mem_write = top.create_cable()
+        mem_write.name = "mem_write"
+        mem_write.create_wires(mem_width)
+
+        mem_read = top.create_cable()
+        mem_read.name = "mem_read"
+        mem_read.create_wires(mem_width)
+
+        proc_i = top.create_child()
+        proc_i.name = "proc_i"
+        proc_i.reference = proc
+        mem_i = top.create_child()
+        mem_i.name = "mem_i"
+        mem_i.reference = mem
+
+        for i in range(mem_width):
+            mem_write.wires[i].connect_pin(mem_i.pins[mem_write_in.pins[i]])
+            mem_write.wires[i].connect_pin(proc_i.pins[mem_write_out.pins[i]])
+            mem_read.wires[i].connect_pin(mem_i.pins[mem_read_out.pins[i]])
+            mem_read.wires[i].connect_pin(proc_i.pins[mem_read_in.pins[i]])
+
+        return mem_write_out, mem_read_in
+
+
+    def create_proc(self, proc, mem_write, mem_read, core, cachel2):
+        mem_width = 10
+        core_i1 = proc.create_child()
+        core_i2 = proc.create_child()
+        core_i1.reference = core
+        core_i2.reference = core
+        core_i1.name = "core_i1"
+        core_i2.name = "core_i2"
+
+        mem_p = core.create_port()
+        mem_p.create_pins(mem_width)
+        mem_p.name = "mem_p"
+
+        cachel2_i = proc.create_child()
+        cachel2_i.reference = cachel2
+        cachel2_i.name = "cachel2_i"
+        
+        mem_a_in  = cachel2.create_port()
+        mem_a_out = cachel2.create_port()
+        mem_b_in  = cachel2.create_port()
+        mem_b_out = cachel2.create_port()
+        
+        
+        mem_a_in.create_pins(mem_width)
+        mem_a_out.create_pins(mem_width)
+        mem_b_in.create_pins(mem_width)
+        mem_b_out.create_pins(mem_width)
+
+        mem_a_in.name = "mem_a_in"
+        mem_b_out.name = "mem_a_out"
+        mem_b_out.name = "mem_b_out"
+        mem_b_in.name = "mem_b_in"
+        
+        
+        ca1 = proc.create_cable()
+        ca2 = proc.create_cable()
+        cb1 = proc.create_cable()
+        cb2 = proc.create_cable()
+
+        ca1.create_wires(mem_width)
+        ca2.create_wires(mem_width)
+        cb1.create_wires(mem_width)
+        cb2.create_wires(mem_width)
+
+        ca1.name = "ca1"
+        ca2.name = "ca2"
+        cb1.name = "cb1"
+        cb2.name = "cb2"
+
+        for i in range(mem_width):
+           ca1.wires[i].connect_pin(core_i1.pins[mem_p.pins[i]])
+           ca1.wires[i].connect_pin(cachel2_i.pins[mem_a_in.pins[i]])
+           ca2.wires[i].connect_pin(mem_read.pins[i])
+           ca2.wires[i].connect_pin(cachel2_i.pins[mem_a_out.pins[i]])
+           cb1.wires[i].connect_pin(core_i2.pins[mem_p.pins[i]])
+           cb1.wires[i].connect_pin(cachel2_i.pins[mem_b_in.pins[i]])
+           cb2.wires[i].connect_pin(mem_write.pins[i])
+           cb2.wires[i].connect_pin(cachel2_i.pins[mem_b_out.pins[i]])
+
+        return mem_p
+
+    def create_core(self, core, mem_p, alu, reg_file):
+        mem_width = 10
+        alu_i = core.create_child()
+        reg_file_i = core.create_child()
+        
+        alu_i.reference = alu
+        alu_i.name = "alu_i"
+        reg_file_i.reference = reg_file
+        reg_file_i.name = "reg_file_i"
+
+        alu_p = alu.create_port()
+        reg_p = reg_file.create_port()
+
+        alu_p.create_pins(mem_width)
+        reg_p.create_pins(mem_width)
+        alu_p.name = "alu_p"
+        reg_p.name = "reg_p"
+
+        mem_cable = core.create_cable()
+
+        mem_cable.create_wires(mem_width)
+        mem_cable.name = "mem_cable"
+
+        for i in range(mem_width):
+            mem_cable.wires[i].connect_pin(reg_file_i.pins[reg_p.pins[i]])
+            mem_cable.wires[i].connect_pin(alu_i.pins[alu_p.pins[i]])
+            mem_cable.wires[i].connect_pin(mem_p.pins[i])
+
+    def create_netlist_with_wires(self):
+        nl = Netlist()
+        lib = nl.create_library()
+        top = lib.create_definition()
+        top.name = "top"
+        proc = lib.create_definition()
+        proc.name = "proc"
+        core = lib.create_definition()
+        core.name = "core"
+        alu = lib.create_definition()
+        alu.name = "alu"
+        mem = lib.create_definition()
+        mem.name = "mem"
+        cachel2 = lib.create_definition()
+        cachel2.name = "cacheL2"
+        reg_file = lib.create_definition()
+        reg_file.name = "reg_file"
+        nl.top_instance = Instance()
+        nl.top_instance.reference = top
+
+        
+        mem_write, mem_read= self.create_mem_bus(top, proc, mem)
+
+        core_mem = self.create_proc(proc, mem_write, mem_read, core, cachel2)
+
+        self.create_core(core, core_mem, alu, reg_file)
+
+        nl.top_instance.name = ("top_instance")
+        
+        #self.simple_recursive_netlist_visualizer(nl)
+
+        return nl
+
+
 
     def simple_recursive_netlist_visualizer(self, netlist):
         #TODO put this code somewhere where people can use it to visualize simple netlists
@@ -67,11 +230,31 @@ class TestUniquify(unittest.TestCase):
         
         recurse(top_instance, 0)
 
+    def simple_cable_connection_visualizer(self,netlist):
+        top_instance = netlist.top_instance
+        #display all the cables in the top instance with all of their connections next to it
+        def recur(instance):
+            print(instance.name)
+            for c in instance.reference.cables:
+                print("\t(cable): ", c.name)
+                if len(c.wires) > 0:
+                    w = c.wires[0]
+                    for p in w.pins:
+                        if isinstance(p,OuterPin):
+                            print("\t\t(instance): ", p.instance.name)
+                        else:
+                            print("\t\t(port):     ", p.port.name)
+            for i in instance.reference.children:
+                recur(i)
+
+        recur(top_instance)
+
+
     def is_flat(self,nl):
         ti = nl.top_instance
         td = ti.reference
         for i in td.children:
-            if not i.is_leaf():
+            if not i.reference.is_leaf():
                 return False
         return True
 
@@ -85,7 +268,13 @@ class TestUniquify(unittest.TestCase):
 
     def test_flatten_cables(self):
         #create a netlist with some connections
-
-        #uniquify the netlist
-
+        nl = self.create_netlist_with_wires()
+        #self.simple_recursive_netlist_visualizer(nl)
+        self.simple_cable_connection_visualizer(nl)
+        
+        uniquify(nl)
+        flatten(nl)
+        #self.simple_recursive_netlist_visualizer(nl)
+        self.simple_cable_connection_visualizer(nl)
+        assert False, "this is incomplete"
         pass
