d37929ba686c56848165651adb06f90dde99c466
thunder-hammer <dmsisawesome@gmail.com>
2020-05-05 12:55:59 -0600

    started modifying the parser and addded some notes to clone

--- spydrnet/clone.py
+++ spydrnet/clone.py
@@ -6,6 +6,21 @@ from copy import deepcopy, copy, error
 def clone(element):
     '''
     Clone any netlist objects
-    see the individaul class .clone methods for more information on what happens when you clone
+
+    several premises hold while cloning
+     * the object will be orphaned and not belong to any parent
+     * the object will maintain internal structure with cloned objects
+     * the names will be unchanged
+     * external connections will mostly be severed
+
+    Properties
+     * cloned using python's built in deepcopy functionality.
+     * expected to be string objects but if you store something else there make sure you override deepcopy on that object.
+    
+    Instances have some special considerations
+     * when cloned without the library containing the reference definition the instance will still point to the definition of it's clone.
+     * in the same case as the above point the references of the definition will be updated accordingly
+     * when a library is cloned some of the instances may be defined in another library these instances will follow the premises above
+     * instances defined and referenced in the cloned library will point to the cloned definition
     '''
     return element.clone()
