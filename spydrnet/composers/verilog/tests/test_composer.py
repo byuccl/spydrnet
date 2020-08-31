f1ee8435f25e67d3e5a6c0f56891a7d2abd5a149
thunder-hammer <dmsisawesome@gmail.com>
2020-08-31 11:06:41 -0600

    fixed some directory issues

--- spydrnet/composers/verilog/tests/test_composer.py
+++ spydrnet/composers/verilog/tests/test_composer.py
@@ -3,61 +3,31 @@ import unittest
 import spydrnet as sdn
 from spydrnet.composers.verilog.composer import Composer
 import os
+import tempfile
 
 class TestVerilogComposer(unittest.TestCase):
 
     def test_simple_compose(self):
-        # #netlist = sdn.load_example_netlist_by_name("hierarchical_luts")
-        # vp = sdn.parsers.verilog.parser.VerilogParser.from_filename("/home/dallin/Documents/byuccl/SpyDrNet/spydrnet/support_files/verilog_netlists/4bitadder.v.zip")
-        # netlist = vp.parse()
-        # comp = Composer()
-        # comp.run(netlist,"out0.v")
-        # vp = sdn.parsers.verilog.parser.VerilogParser.from_filename("/home/dallin/Documents/byuccl/SpyDrNet/spydrnet/support_files/verilog_netlists/8051.v.zip")
-        # netlist = vp.parse()
-        # comp = Composer()
-        # comp.run(netlist,"out1.v")
-        # vp = sdn.parsers.verilog.parser.VerilogParser.from_filename("/home/dallin/Documents/byuccl/SpyDrNet/spydrnet/support_files/verilog_netlists/adder.v.zip")
-        # netlist = vp.parse()
-        # comp = Composer()
-        # comp.run(netlist,"out2.v")
-        # # vp = sdn.parsers.verilog.parser.VerilogParser.from_filename("/home/dallin/Documents/byuccl/SpyDrNet/spydrnet/support_files/verilog_netlists/b13.v.zip")
-        # # netlist = vp.parse()
-        # # comp = Composer()
-        # # comp.run(netlist,"out3.v")
-        # vp = sdn.parsers.verilog.parser.VerilogParser.from_filename("/home/dallin/Documents/byuccl/SpyDrNet/spydrnet/support_files/verilog_netlists/basic_clock_crossing.v.zip")
-        # netlist = vp.parse()
-        # comp = Composer()
-        # comp.run(netlist,"out4.v")
-        # vp = sdn.parsers.verilog.parser.VerilogParser.from_filename("/home/dallin/Documents/byuccl/SpyDrNet/spydrnet/support_files/verilog_netlists/basic_synchronizer.v.zip")
-        # netlist = vp.parse()
-        # comp = Composer()
-        # comp.run(netlist,"out5.v")
-        # # vp = sdn.parsers.verilog.parser.VerilogParser.from_filename("/home/dallin/Documents/byuccl/SpyDrNet/spydrnet/support_files/verilog_netlists/bram.v.zip")
-        # # netlist = vp.parse()
-        # # comp = Composer()
-        # # comp.run(netlist,"out6.v")
-        # # vp = sdn.parsers.verilog.parser.VerilogParser.from_filename("/home/dallin/Documents/byuccl/SpyDrNet/spydrnet/support_files/verilog_netlists/carrychain.v.zip")
-        # # netlist = vp.parse()
-        # # comp = Composer()
-        # # comp.run(netlist,"out7.v")
 
         i = 0
         errors = 0
-        directory = "spydrnet/support_files/verilog_netlists/"
+        directory = os.path.join(sdn.base_dir, "support_files", "verilog_netlists")
         for filename in os.listdir(directory):
             if filename.endswith(".zip"):
-                try:
-                    print("*********************"+filename+"*********************")
-                    vp = sdn.parsers.verilog.parser.VerilogParser.from_filename(directory + filename)
-                    netlist = vp.parse()
-                    comp = Composer()
-                    comp.run(netlist,"temp2/"+filename[:len(filename)-6] + "-spydrnet.v")
-                    i+=1
-                    print("pass")
-                except Exception as identifier:
-                    print("FAIL")
-                    print(identifier)
-                    errors += 1
+                with tempfile.TemporaryDirectory() as tempdirectory:
+                    # try:
+                        print("*********************"+filename+"*********************")
+                        vp = sdn.parsers.verilog.parser.VerilogParser.from_filename(os.path.join(directory, filename))
+                        netlist = vp.parse()
+                        comp = Composer()
+                        #comp.run(netlist,"temp2/"+filename[:len(filename)-6] + "-spydrnet.v")
+                        comp.run(netlist,os.path.join(tempdirectory, filename[:len(filename)-6] + "-spydrnet.v"))
+                        i+=1
+                        print("pass")
+                    # except Exception as identifier:
+                    #     print("FAIL")
+                    #     print(identifier)
+                    #     errors += 1
             else:
                 continue
 
