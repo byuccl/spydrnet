5e26b038c4d0c4c7fd8656d41027c0939e523041
thunder-hammer <skousond@byu.edu>
2021-03-17 20:13:58 -0600

    added some code to help simplify the verilog tokenizer

---
+++ spydrnet/parsers/verilog/verilog_token_factory.py
@@ -0,0 +1,102 @@
+import spydrnet.parsers.verilog.verilog_tokens as vt
+
+class TokenFactory():
+    def __init__(self):    
+        self.buffer = ''
+        #only one of these should ever be true at any time.
+        self.single_line_comment = False
+        self.multi_line_comment = False
+        self.string = False
+        self.escaped_identifier = False
+        self.processor_directive = False
+        self.last_character = "" #should not be relied on to always be the last character when tokens are ending
+    
+    def clear(self, character = ""):
+        self.buffer = character
+
+    def any_flag_set(self):
+        return self.single_line_comment or self.multi_line_comment or self.string or self.escaped_identifier or self.processor_directive
+
+    def set_flags(self):
+        if len(self.buffer) <= 2 and not self.any_flag_set():
+            
+            if self.buffer == vt.OPEN_LINE_COMMENT:
+                self.single_line_comment = True
+        
+            elif self.buffer == vt.OPEN_BLOCK_COMMENT:
+                self.multi_line_comment = True
+        
+            elif self.buffer == '"':
+                self.string = True
+        
+            elif self.buffer == "\\":
+                self.escaped_identifier = True
+            
+            elif self.buffer == "`":
+                self.processor_directive = True
+
+    def add_character(self, character):
+        #tries to add the character to the token.
+        #if the token is complete it will return and start the next token
+        #if the token is incomplete it will add it and return none
+        token_out = None
+        #first thing, see if we need to return a token
+        if self.buffer in vt.SINGLE_CHARACTER_TOKENS:
+            token_out = self.buffer
+            self.clear()
+
+        elif self.single_line_comment and character == vt.NEW_LINE:
+            token_out = self.buffer
+            self.clear()
+            self.single_line_comment = False
+
+        elif self.multi_line_comment and self.last_character == "*" and character == "/":
+            token_out = self.buffer + character
+            character = ""
+            self.clear()
+            self.multi_line_comment = False
+
+        elif self.escaped_identifier and character in vt.WHITESPACE:
+            token_out = self.buffer + " "
+            self.clear()
+            self.escaped_identifier = False
+
+        elif self.string and character == '"':
+            token_out = self.buffer + character
+            character = ""
+            self.clear()
+            self.string = False
+
+        elif self.processor_directive and character == vt.NEW_LINE:
+            token_out = self.buffer
+            self.clear()
+            self.processor_directive = False
+
+        elif character == vt.STAR and self.last_character == "/":
+            pass #this will be a multi line comment but * is a breaker...
+
+        elif character in vt.BREAKER_TOKENS and not self.any_flag_set() and len(self.buffer) != 0:
+            token_out = self.buffer
+            self.clear()
+
+        
+        if character not in vt.WHITESPACE:    
+            self.buffer = self.buffer + character
+        elif self.single_line_comment or self.multi_line_comment or self.string or self.processor_directive:
+            self.buffer = self.buffer + character
+        self.last_character = character
+        
+        last_multi_line_comment = self.multi_line_comment
+        self.set_flags()
+        if self.multi_line_comment and not last_multi_line_comment:
+            self.last_character = "" #these lines help to ensure that /*/ isn't precieved as a complete comment.
+
+
+        return token_out
+
+    def flush(self):
+        token_out = self.buffer
+        self.clear()
+        if token_out == "":
+            return None
+        return token_out
--- spydrnet/parsers/verilog/verilog_tokens.py
+++ spydrnet/parsers/verilog/verilog_tokens.py
@@ -1,34 +1,97 @@
-LEFT_PAREN = '('
-RIGHT_PAREN = ')'
+
+TAB = '\t'
+NEW_LINE ='\n'
+CARRIAGE_RETURN = '\r'
+SPACE = ' '
+FORM_FEED = '\f'
+
+NUMBERS = {'0','1','2','3','4','5','6','7','8','9'}
+LETTERS = {'a','b','c','d','e','f','g','h','i','j',\
+    'k','l','m','n','o','p','q','r','s','t','u','v',\
+    'w','x','y','z',\
+    'A','B','C','D','E','F','G','H',\
+    'I','J','K','L','M','N','O','P','Q','R','S','T',\
+    'U','V','W','X','Y','Z'}
+
+WHITESPACE = {SPACE, TAB, NEW_LINE, CARRIAGE_RETURN, FORM_FEED}
+
+OPEN_PARENTHESIS = '('
+CLOSE_PARENTHESIS = ')'
 STAR = '*'
 SEMI_COLON = ';'
-BACKSLASH = '\\'
-INVERTER = '~'
 MODULE = "module"
 INPUT = "input"
 OUTPUT = "output"
 INOUT = "inout"
 WIRE = "wire"
 REG = "reg"
-STRING = "string"
-COMMENT = "comment"
-IDENTIFIER = "identifier"
 DOT = "."
-LEFT_BRACKET = "["
-RIGHT_BRACKET = "]"
+OPEN_BRACKET = "["
+CLOSE_BRACKET = "]"
+OPEN_BRACE = "{"
+CLOSE_BRACE = "}"
 COLON = ":"
 COMMA = ","
-DOUBLE_QUOTE = '"'
 OCTOTHORP = "#"
-NUMBER = "number"
 END_MODULE = "endmodule"
-BACKTICK = "`"
 PARAMETER = "parameter"
 SINGLE_QUOTE = "'"
 LOCAL_PARAM = "localparam"
 ASSIGN = "assign"
 EQUAL = "="
-CELL_DEFINE = "celldefine"
-IF_DEF = "ifdef"
-ENDIF = 'endif'
-EOF = "eof"
+CELL_DEFINE = "`celldefine"
+IFDEF = "`ifdef"
+ENDIF = '`endif'
+ELSIF = '`elsif'
+TIMESCALE = '`timescale'
+OPEN_BLOCK_COMMENT = "/*"
+CLOSE_BLOCK_COMMENT = "*/"
+OPEN_LINE_COMMENT = "//"
+
+#SET OF ALL THINGS THAT WILL END AN IDENTIFIER IF THEY ARE NOT ESCAPED.
+#elif ch in {'(', ')', '.', ',', ';', '[', ']', ':', "{", "}", "*", "#", "`", "'", }:
+BREAKER_TOKENS = {SPACE, TAB, NEW_LINE, CARRIAGE_RETURN, FORM_FEED, OPEN_PARENTHESIS, CLOSE_PARENTHESIS, DOT,\
+    COMMA, SEMI_COLON, OPEN_BRACKET, CLOSE_BRACKET, COLON, OPEN_BRACE, CLOSE_BRACE, STAR, OCTOTHORP,\
+        SINGLE_QUOTE, EQUAL, "\\", "\"", "`"}
+
+SINGLE_CHARACTER_TOKENS = {OPEN_PARENTHESIS, CLOSE_PARENTHESIS, STAR, SEMI_COLON, DOT, OPEN_BRACKET,\
+    CLOSE_BRACKET, OPEN_BRACE, CLOSE_BRACE, COLON, COMMA, COMMA, OCTOTHORP, SINGLE_QUOTE, EQUAL}
+
+ALL_VERILOG_TOKENS = {
+    TAB,\
+    NEW_LINE,\
+    CARRIAGE_RETURN,\
+    SPACE,\
+    FORM_FEED,\
+    OPEN_PARENTHESIS,\
+    CLOSE_PARENTHESIS,\
+    STAR,\
+    SEMI_COLON,\
+    MODULE,\
+    INPUT,\
+    OUTPUT,\
+    INOUT,\
+    WIRE,\
+    REG,\
+    DOT,\
+    OPEN_BRACKET,\
+    CLOSE_BRACKET,\
+    OPEN_BRACE,\
+    CLOSE_BRACE,\
+    COLON,\
+    COMMA,\
+    OCTOTHORP,\
+    END_MODULE,\
+    PARAMETER,\
+    SINGLE_QUOTE,\
+    LOCAL_PARAM,\
+    ASSIGN,\
+    EQUAL,\
+    CELL_DEFINE,\
+    IFDEF,\
+    ENDIF,\
+    ELSIF,\
+    OPEN_BLOCK_COMMENT,\
+    CLOSE_BLOCK_COMMENT,\
+    OPEN_LINE_COMMENT,\
+}
