c6044da537f70cc52c17c39de85bde061aaf3f3d
jacob <jacobb00@byu.edu>
2022-02-10 22:29:29 -0700

    start of eblif parser tests

---
+++ spydrnet/parsers/eblif/tests/test_eblif_parser.py
@@ -0,0 +1,59 @@
+import unittest
+import os
+import spydrnet as sdn
+from spydrnet import base_dir
+from spydrnet.parsers.eblif.eblif_parser import EBLIFParser
+
+"""
+Test the EBLIFParser by parsing in a simple netlist and making sure that:
+    - Everything is there
+    - Everything is working together as a netlist as it should (to be sure things were created and placed in the netlist correctly)
+"""
+class TestEBLIFParser(unittest.TestCase):
+    def setUp(self):
+        self.netlist = sdn.parse(os.path.join(base_dir, 'support_files', 'eblif_netlists', "toggle.eblif.zip"))
+        self.definition_list = ["INV","BUFG","FDRE","IBUF","OBUF","toggle"]
+
+    def test_name(self):
+        self.assertEqual(self.netlist.name,"toggle")
+
+    def test_definitions(self):
+        count = 0
+        for definition in self.netlist.get_definitions():
+            self.assertTrue(definition.name in self.definition_list,definition.name +" not found in list")
+            self.assertTrue(definition)
+            count+=1
+        self.assertEqual(count,6)
+    
+    def test_instances(self):
+        self.assertEqual(self.netlist.top_instance.name,"toggle")
+        for instance in self.netlist.get_instances():
+            self.assertTrue(instance.reference.name in self.definition_list)
+            self.assertTrue(self.netlist_is_own_netlist(instance))
+
+    def test_top_level_ports(self):
+        input_port_list = ["clk","reset"]
+        output_port_list = ["out"]
+        for port in self.netlist.get_hports():
+            if port.item.direction is sdn.IN:
+                self.assertTrue(port.item.name in input_port_list,port.item.name+" is not in "+str(input_port_list))
+                self.assertTrue(self.netlist_is_own_netlist(port))
+            elif port.item.direction is sdn.OUT:
+                self.assertTrue(port.item.name in output_port_list,port.item.name+" is not in "+str(output_port_list))
+                self.assertTrue(self.netlist_is_own_netlist(port))
+
+    def test_cables(self):
+        None
+
+    def test_wire_and_pin_connections(self):
+        None
+
+    def netlist_is_own_netlist(self,object):
+        netlist_list = list(x for x in object.get_netlists())
+        if (self.netlist in netlist_list):
+            return True
+        return False
+        
+
+# file = os.path.join(base_dir, 'support_files', 'eblif_netlists', "toggle.eblif.zip")
+# netlist = sdn.parse(file)
