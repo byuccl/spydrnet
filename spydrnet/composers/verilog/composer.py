e87b732114e007147f775c8b607dcfc9e64d1fd4
Jacob Brown <jacobb00@byu.edu>
2022-05-09 09:21:33 -0600

    added new defparam parameter to parameter list

--- spydrnet/composers/verilog/composer.py
+++ spydrnet/composers/verilog/composer.py
@@ -14,6 +14,7 @@ class Composer:
 
         definition_list - (list[str]) list of defintions to write
         write_blackbox - (bool) Skips writing black boxes/verilog primitives
+        defparam - (bool) Compose parameters in *defparam* statements instead of using #()
         """
         self.file = None
         self.direction_string_map = dict()
