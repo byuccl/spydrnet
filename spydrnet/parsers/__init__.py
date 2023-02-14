dad8359e039fbf66d88bb0a66b56897635345025
Ethan Monlux <ethanjmonlux@gmail.com>
2023-02-14 11:13:35 -0700

    committing changes

--- spydrnet/parsers/__init__.py
+++ spydrnet/parsers/__init__.py
@@ -35,14 +35,9 @@ def parse(filename, architecture=None, remove_space=False):
 
     The same applies for EBLIF files
     """
-    
     basename_less_final_extension = os.path.splitext(
         os.path.basename(filename))[0]
     extension = get_lowercase_extension(filename)
-    # filename = os.path.basename(filename)
-    print('FILENAME ',filename)
-    print('BASENAME ',basename_less_final_extension)
-    print('EXTENSION',extension)
     if extension == ".zip":
         assert zipfile.is_zipfile(filename), \
             "Input filename {} with extension .zip is not a zip file.".format(
@@ -55,10 +50,31 @@ def parse(filename, architecture=None, remove_space=False):
                 zip.extract(basename_less_final_extension, tempdirname)
                 filename = os.path.join(
                     tempdirname, basename_less_final_extension)
-                return _parse(filename, architecture, remove_space)
-    filename = os.path.basename(filename)
-    print("FINAL",filename)
+                return _parse(filename)
     return _parse(filename, architecture, remove_space)
+    # basename_less_final_extension = os.path.splitext(
+    #     os.path.basename(filename))[0]
+    # extension = get_lowercase_extension(filename)
+    # # filename = os.path.basename(filename)
+    # print('FILENAME ',filename)
+    # print('BASENAME ',basename_less_final_extension)
+    # print('EXTENSION',extension)
+    # if extension == ".zip":
+    #     assert zipfile.is_zipfile(filename), \
+    #         "Input filename {} with extension .zip is not a zip file.".format(
+    #             basename_less_final_extension)
+    #     with tempfile.TemporaryDirectory() as tempdirname:
+    #         with zipfile.ZipFile(filename) as zip:
+    #             files = zip.namelist()
+    #             assert len(files) == 1 and files[0] == basename_less_final_extension, \
+    #                 "Only single file archives allowed with a file whose name matches the name of the archive"
+    #             zip.extract(basename_less_final_extension, tempdirname)
+    #             filename = os.path.join(
+    #                 tempdirname, basename_less_final_extension)
+    #             return _parse(filename, architecture, remove_space)
+    # filename = os.path.basename(filename)
+    # print("FINAL",filename)
+    # return _parse(filename, architecture, remove_space)
 
 
 def _parse(filename, architecture=None, remove_space=False):
